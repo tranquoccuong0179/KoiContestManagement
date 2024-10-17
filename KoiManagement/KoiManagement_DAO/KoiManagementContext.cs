3ba7ffe789fbb9e826dcaf4f8d0d98871f76eeee
Nguyễn Lê Tấn Nghiệp <tannghiepnguyen123@gmail.com>
2024-10-17 14:56:37 +0700

    Adding identity to user entity

--- KoiManagement/KoiManagement_DAO/KoiManagementContext.cs
+++ KoiManagement/KoiManagement_DAO/KoiManagementContext.cs
@@ -1,120 +1,109 @@
 ﻿using KoiManagement_BusinessObjects;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace KoiManagement_DAO;
 
-public partial class KoiManagementContext : DbContext
+public partial class KoiManagementContext : IdentityDbContext
 {
-    public KoiManagementContext()
-    {
-    }
+	public KoiManagementContext()
+	{
+	}
 
-    public KoiManagementContext(DbContextOptions<KoiManagementContext> options)
-        : base(options)
-    {
-    }
+	public KoiManagementContext(DbContextOptions<KoiManagementContext> options)
+		: base(options)
+	{
+	}
 
-    public virtual DbSet<Achievement> Achievements { get; set; }
+	public virtual DbSet<Achievement> Achievements { get; set; }
 
-    public virtual DbSet<Category> Categories { get; set; }
+	public virtual DbSet<Category> Categories { get; set; }
 
-    public virtual DbSet<Competition> Competitions { get; set; }
+	public virtual DbSet<Competition> Competitions { get; set; }
 
-    public virtual DbSet<CompetitionCategory> CompetitionCategories { get; set; }
+	public virtual DbSet<CompetitionCategory> CompetitionCategories { get; set; }
 
-    public virtual DbSet<CompetitionRound> CompetitionRounds { get; set; }
+	public virtual DbSet<CompetitionRound> CompetitionRounds { get; set; }
 
-    public virtual DbSet<CriteriaPoint> CriteriaPoints { get; set; }
+	public virtual DbSet<CriteriaPoint> CriteriaPoints { get; set; }
 
-    public virtual DbSet<Criteria> Criteria { get; set; }
+	public virtual DbSet<Criteria> Criteria { get; set; }
 
-    public virtual DbSet<Koi> Kois { get; set; }
+	public virtual DbSet<Koi> Kois { get; set; }
 
-    public virtual DbSet<Mark> Marks { get; set; }
+	public virtual DbSet<Mark> Marks { get; set; }
 
-    public virtual DbSet<Prediction> Predictions { get; set; }
+	public virtual DbSet<Prediction> Predictions { get; set; }
 
-    public virtual DbSet<RefereeMark> RefereeMarks { get; set; }
+	public virtual DbSet<RefereeMark> RefereeMarks { get; set; }
 
-    public virtual DbSet<Registration> Registrations { get; set; }
+	public virtual DbSet<Registration> Registrations { get; set; }
 
-    public virtual DbSet<Result> Results { get; set; }
+	public virtual DbSet<Result> Results { get; set; }
 
-    public virtual DbSet<Role> Roles { get; set; }
+	public virtual DbSet<Round> Rounds { get; set; }
 
-    public virtual DbSet<Round> Rounds { get; set; }
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
+		modelBuilder.Entity<CriteriaPoint>(entity =>
+		{
+			entity.HasKey(cp => new { cp.RefereeMarkId, cp.CriteriaId });
+		});
+		modelBuilder.Entity<Prediction>(entity =>
+		{
+			entity.HasKey(p => p.Id);
 
-    public virtual DbSet<User> Users { get; set; }
+			entity.HasOne(p => p.User)
+				  .WithMany(u => u.Predictions)
+				  .HasForeignKey(p => p.UserId)
+				  .OnDelete(DeleteBehavior.NoAction);
 
-    public virtual DbSet<UserRole> UserRoles { get; set; }
+			entity.HasOne(p => p.CompetitionRound)
+				  .WithMany(cr => cr.Predictions)
+				  .HasForeignKey(p => p.CompetitionRoundId)
+				  .OnDelete(DeleteBehavior.Cascade);
+		});
+		modelBuilder.Entity<RefereeMark>(entity =>
+		{
+			entity.HasKey(p => p.Id);
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        => optionsBuilder.UseSqlServer("Server=(local);uid=sa;pwd=12345678;database=KoiManagement;Trusted_Connection=True;TrustServerCertificate=True;");
+			entity.HasOne(p => p.User)
+				  .WithMany(u => u.RefereeMarks)
+				  .HasForeignKey(p => p.UserId)
+				  .OnDelete(DeleteBehavior.NoAction);
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        modelBuilder.Entity<CriteriaPoint>(entity =>
-        {
-            entity.HasKey(cp => new { cp.RefereeMarkId, cp.CriteriaId });
-        });
-        modelBuilder.Entity<UserRole>(entity =>
-        {
-            entity.HasKey(ur => new { ur.UserId, ur.RoleId });
-        });
-        modelBuilder.Entity<Prediction>(entity =>
-        {
-            entity.HasKey(p => p.Id);
+			entity.HasOne(p => p.CompetitionRound)
+				  .WithMany(u => u.RefereeMarks)
+				  .HasForeignKey(p => p.CompetitionRoundId)
+				  .OnDelete(DeleteBehavior.NoAction);
+			entity.HasOne(p => p.Mark)
+				  .WithMany(u => u.RefereeMarks)
+				  .HasForeignKey(p => p.MarkId)
+				  .OnDelete(DeleteBehavior.NoAction);
+		});
+		modelBuilder.Entity<Achievement>(entity =>
+		{
+			entity.HasKey(p => p.Id);
 
-            entity.HasOne(p => p.User)
-                  .WithMany(u => u.Predictions)
-                  .HasForeignKey(p => p.UserId)
-                  .OnDelete(DeleteBehavior.NoAction); 
+			entity.HasOne(p => p.Result)
+				  .WithMany(u => u.Achievements)
+				  .HasForeignKey(p => p.ResultId)
+				  .OnDelete(DeleteBehavior.NoAction);
 
-            entity.HasOne(p => p.CompetitionRound)
-                  .WithMany(cr => cr.Predictions)
-                  .HasForeignKey(p => p.CompetitionRoundId)
-                  .OnDelete(DeleteBehavior.Cascade); 
-        });
-        modelBuilder.Entity<RefereeMark>(entity =>
-        {
-            entity.HasKey(p => p.Id);
+			entity.HasOne(p => p.Koi)
+				  .WithMany(cr => cr.Achievements)
+				  .HasForeignKey(p => p.KoiId)
+				  .OnDelete(DeleteBehavior.Cascade);
+		});
+		modelBuilder.Entity<Mark>(entity =>
+		{
+			entity.HasKey(p => p.Id);
 
-            entity.HasOne(p => p.User)
-                  .WithMany(u => u.RefereeMarks)
-                  .HasForeignKey(p => p.UserId)
-                  .OnDelete(DeleteBehavior.NoAction);
-
-            entity.HasOne(p => p.CompetitionRound)
-                  .WithMany(u => u.RefereeMarks)
-                  .HasForeignKey(p => p.CompetitionRoundId)
-                  .OnDelete(DeleteBehavior.NoAction);
-            entity.HasOne(p => p.Mark)
-                  .WithMany(u => u.RefereeMarks)
-                  .HasForeignKey(p => p.MarkId)
-                  .OnDelete(DeleteBehavior.NoAction);
-        });
-        modelBuilder.Entity<Achievement>(entity =>
-        {
-            entity.HasKey(p => p.Id);
-
-            entity.HasOne(p => p.Result)
-                  .WithMany(u => u.Achievements)
-                  .HasForeignKey(p => p.ResultId)
-                  .OnDelete(DeleteBehavior.NoAction);
-
-            entity.HasOne(p => p.Koi)
-                  .WithMany(cr => cr.Achievements)
-                  .HasForeignKey(p => p.KoiId)
-                  .OnDelete(DeleteBehavior.Cascade);
-        });
-        modelBuilder.Entity<Mark>(entity =>
-        {
-            entity.HasKey(p => p.Id);
-
-            entity.HasOne(p => p.CompetitionRound)
-                  .WithMany(u => u.Marks)
-                  .HasForeignKey(p => p.CompetitionRoundId)
-                  .OnDelete(DeleteBehavior.NoAction);
-        });
-    }
+			entity.HasOne(p => p.CompetitionRound)
+				  .WithMany(u => u.Marks)
+				  .HasForeignKey(p => p.CompetitionRoundId)
+				  .OnDelete(DeleteBehavior.NoAction);
+		});
+	}
 }
--- KoiManagement/KoiManagement_Services/Service/ServiceManager.cs
+++ KoiManagement/KoiManagement_Services/Service/ServiceManager.cs
@@ -1,18 +1,21 @@
-﻿using KoiManagement_Repositories.IRepository;
+﻿using AutoMapper;
+using KoiManagement_Repositories.IRepository;
 using KoiManagement_Service.IService;
 using Microsoft.Extensions.Configuration;
 
 namespace KoiManagement_Service.Service
 {
-    public class ServiceManager : IServiceManager
-    {
-        private readonly IRepositoryManager repositoryManager;
-        private readonly IConfiguration configuration;
+	public class ServiceManager : IServiceManager
+	{
+		private readonly IRepositoryManager repositoryManager;
+		private readonly IConfiguration configuration;
+		private readonly IMapper mapper;
 
-        public ServiceManager(IRepositoryManager repositoryManager, IConfiguration configuration)
-        {
-            this.repositoryManager = repositoryManager;
-            this.configuration = configuration;
-        }
-    }
+		public ServiceManager(IRepositoryManager repositoryManager, IConfiguration configuration, IMapper mapper)
+		{
+			this.repositoryManager = repositoryManager;
+			this.configuration = configuration;
+			this.mapper = mapper;
+		}
+	}
 }
