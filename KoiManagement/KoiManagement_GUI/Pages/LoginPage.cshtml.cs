596c85658a901d22b7fa437887477b07b8778751
Nguyễn Lê Tấn Nghiệp <tannghiepnguyen123@gmail.com>
2024-10-17 14:21:53 +0700

    Finish design Login page and Signup page

---
+++ KoiManagement/KoiManagement_GUI/Pages/LoginPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiManagement_GUI.Pages
+{
+    public class LoginPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ KoiManagement/KoiManagement_GUI/Pages/SignupPage.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace KoiManagement_GUI.Pages
+{
+    public class SignupPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
